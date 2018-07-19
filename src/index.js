3758ea9c23f25f28722dfbac5950c24addd28580
Alan Charles <alanjcharles14@gmail.com>
2018-07-19 14:04:36 -0600

    Add router and redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@
 
 import React from 'react'
 import { render } from 'react-dom'
-
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from '../src/reducers'
-
 import App from './containers/App/App'
 
-const store = createStore(rootReducer)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(rootReducer, devTools)
 
 render(
   <Provider store={ store } >
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
