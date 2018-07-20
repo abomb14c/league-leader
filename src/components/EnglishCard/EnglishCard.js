94309d6b36eb2fda84cdd7ceae5e238201efa02a
Alan Charles <alanjcharles14@gmail.com>
2018-07-19 20:04:16 -0600

    Add english card basic styling

--- src/components/EnglishCard/EnglishCard.js
+++ src/components/EnglishCard/EnglishCard.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import './english-card.css'
 
 export const EnglishCard = (props) => {
-  const {name, 
+  const { name, 
     wins,
     losses,
     draws,
     points,
-    rank} = props; 
+    rank } = props; 
 
   return (
-    <div>
-      <h3>{name}</h3>
-      <h3>{rank}</h3>
+    <div className="english-card">
+      <h3 className="english-title">{name}</h3>
+      <h3 className="english-rank">{rank}</h3>
+      <h3 className="english-wins">{wins}</h3>
+      <h3 className="english-losses">{losses}</h3>
+      <h3 className="english-points">{points}</h3>
     </div>
   )
 }
