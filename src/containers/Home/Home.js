afe71bd70cd25fbc1c414b957bdb169a9ed7bcdb
Alan Charles <alanjcharles14@gmail.com>
2018-07-19 20:47:58 -0600

    add english card container styling

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import EnglishCard from '../../components/EnglishCard/EnglishCard';
+import './home.css';
 
 export const Home = (props) => {
   const eplCards = props.EPL.map((team, index) => {
@@ -10,8 +11,16 @@ export const Home = (props) => {
   })
 
   return (
-    <div>
-      {eplCards}
+    <div className="home-container">
+      <div className="epl-container">
+        <div className="epl-header">
+          <h3 className="header-desc">Name</h3>
+          <h3 className="header-desc rank">Rank</h3>
+          <h3 className="header-desc record">Record</h3>
+          <h3 className="header-desc points">Points</h3>
+        </div>
+        {eplCards}
+      </div>
     </div>
   )
 }
