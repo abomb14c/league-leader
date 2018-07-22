5a1f4a730bb94e3e4709c8d21a6c544afca73d48
Alan Charles <alanjcharles14@gmail.com>
2018-07-22 15:21:53 -0600

    Add drop down to setUp League

--- src/containers/SetupLeague/SetupLeague.js
+++ src/containers/SetupLeague/SetupLeague.js
@@ -6,18 +6,55 @@ export class SetupLeague extends Component {
     super(props)
 
     this.state = {
+      showMenu: false
+    }
+  }
 
+  showMenu = (event) => {
+    event.preventDefault();
+    
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+  
+  closeMenu = (event) => {
+    if (!this.dropdownMenu.contains(event.target)) {
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });   
     }
   }
 
   render() {
     return (
       <div>
-        create
+        <button onClick={this.showMenu}>
+          Pick A League
+        </button>
+        
+        {
+          this.state.showMenu
+            ? (
+              <div
+                className="menu"
+                ref={(element) => {
+                  this.dropdownMenu = element;
+                }}
+              >
+                <button>English Premier League</button>
+                <button>NBA</button>
+              </div>
+            )
+            : (
+              null
+            )
+        }
       </div>
-    )
+    );
   }
 }
+
 
 export const mapStateToProps = state => ({
  EPL: state.EPL
