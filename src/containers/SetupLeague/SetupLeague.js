f28934a44b6ada7b754d5c4cbd89fb48af032093
Alan Charles <alanjcharles14@gmail.com>
2018-07-22 14:49:06 -0600

    Add SetupLeague container and file structure

---
+++ src/containers/SetupLeague/SetupLeague.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'; 
+
+export class SetupLeauge extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        create
+      </div>
+    )
+  }
+}
+
+export default SetupLeague; 
