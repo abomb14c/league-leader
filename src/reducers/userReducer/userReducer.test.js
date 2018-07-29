245f9fd24be8f85b1768b41a74d5a7feb20f44dd
Alan Charles <alanjcharles14@gmail.com>
2018-07-29 14:30:36 -0600

    Add user reducer tests

---
+++ src/reducers/userReducer/userReducer.test.js
@@ -0,0 +1,46 @@
+import userReducer from './userReducer';
+
+describe('userReducer', () => {
+  it('should return the initial state', () => {
+
+    const expected = {};
+
+    const result = userReducer(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return user info on action.type of "ADD_USER', () => {
+    
+    const mockState = {};
+
+    const user = {userId: '3'};
+
+    const mockAction = {
+      type:"ADD_USER",
+      userId: user.userId
+    };
+
+    const expected = {user_id: mockAction.userId};
+
+    const result = userReducer(mockState, mockAction);
+
+    expect(result).toEqual(expected);
+  });
+
+  it.skip('should return an empty object on action.type "LOGOUT_USER"', () => {
+
+    const mockState = {};
+
+    const mockAction = {
+      type: "LOGOUT_USER"
+    };
+
+    const expected = {};
+
+    const result = userReducer(mockState, mockAction);
+
+    expect(result).toEqual(expected);
+
+  });
+});
