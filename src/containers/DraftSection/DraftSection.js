058ac734ea142faa60db881e397932414412ffbb
Alan Charles <alanjcharles14@gmail.com>
2018-07-31 15:50:25 -0600

    make draft section a class and move draftcards to own container

--- src/containers/DraftSection/DraftSection.js
+++ src/containers/DraftSection/DraftSection.js
@@ -1,49 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import DraftCard from '../../components/DraftCard/DraftCard';
+
 import './draft-section.css';
 import UserDraftCard from '../UserDraftCard/UserDraftCard';
-import { Draggable } from 'react-drag-and-drop';
+import DraftTeamsContainer from '../DraftTeamsContainer/DraftTeamsContainer';
 import PropTypes from 'prop-types';
 
-export const DraftSection = (props) => {
-  const {
-    league,
-    draftTeams
-  } = props;
+export class DraftSection extends Component {
+  constructor(props) {
+    super(props);
+  }
+ 
 
-  const displayDraftTeams = draftTeams.map((team, index) => {
-   
+  render() {
     return (
-      <Draggable type="team" data={team.name} key={index} >
-        <DraftCard name={team.name}  />
-      </Draggable>
-    );
-  });
-
-  return (
-    <div className="draft-container">
-      <div className="league-info-container">
-        <h1 className="league-title">{league.name}</h1>
-        <div className="league-bet-container">
-          <h3 className="league-bet-desc">Wager:</h3>
-          <h3 className="league-bet">{league.bet}</h3>
-        </div>
-      </div>
-      <button className="submit-league-button">Submit League</button>
-      <div className="draft-section">
-        <div className="draft-teams">
-          <div className="draft-teams-title">
-            <h3 className="title-desc">Available Teams</h3>
+      <div className="draft-container">
+        <div className="league-info-container">
+          <h1 className="league-title">{this.props.league.name}</h1>
+          <div className="league-bet-container">
+            <h3 className="league-bet-desc">Wager:</h3>
+            <h3 className="league-bet">{this.props.league.bet}</h3>
           </div>
-          {displayDraftTeams}
         </div>
-        <UserDraftCard />
+        <button className="submit-league-button">Submit League</button>
+        <div className="draft-section">
+          <div className="draft-teams">
+            <div className="draft-teams-title">
+              <h3 className="title-desc">Available Teams</h3>
+            </div>
+            <DraftTeamsContainer /> 
+          </div>
+          <UserDraftCard />
+        </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 
 export const mapStateToProps = state => ({
   league: state.league,
