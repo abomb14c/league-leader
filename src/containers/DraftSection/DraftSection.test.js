565e2ae92c7953b80b991eee9c059767cb2794a4
Alan Charles <alanjcharles14@gmail.com>
2018-07-31 20:35:03 -0600

    Add mdtp test to DraftSection

--- src/containers/DraftSection/DraftSection.test.js
+++ src/containers/DraftSection/DraftSection.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-import  { DraftSection, mapStateToProps}  from './DraftSection';
+import  { DraftSection, mapStateToProps, mapDispatchToProps}  from './DraftSection';
 
 describe('DraftCard', () => {
   let wrapper;
@@ -42,5 +42,27 @@ describe('mapStateToProps', () => {
     
     const mappedProps = mapStateToProps(mockState);
     expect(mappedProps).toEqual(expected);
+  });
+
+  describe('mapDispatchtoProps', () => {
+    it('handleLeagueID should be called with the correct params', async () => {
+
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      const mockLeagueID = {
+        id:2
+      };
+
+      const mockAction = {
+        type: "ADD_LEAGUE_ID", 
+        leagueID:mockLeagueID
+      };
+
+  
+      mappedProps.handleLeagueID(mockLeagueID);
+
+      expect(mockDispatch).toHaveBeenCalledWith(mockAction);
+    });
   });
 });
